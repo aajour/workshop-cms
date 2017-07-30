7e96e760d6f5939e1a36af761c711410833f6395
aajour <ahmed.ajourr@gmail.com>
2017-07-30 15:35:47 +0300

    using fs

--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
 var http = require('http');
-var server = http.createServer();
+var fs = require('fs')
+var message = "Hello world! :D"
+var server = http.createServer(handler);
 server.listen(5000, function() {
     console.log("it's working!");
 })
+
+function handler(req, res) {
+    var endpoint = req.url;
+    if (endpoint == '/') {
+        res.writeHead(200, { 'Conten-Type': 'text/html' });
+        fs.readFile(__dirname + '/../public/index.html', function(err, content) {
+            if (err) {
+                res.end("<h1> Server down </h1>")
+                return;
+            }
+            res.end(content);
+        })
+    }
+    if (endpoint == '/girl') {
+        res.end("I'm a Girl :D")
+    }
+}
