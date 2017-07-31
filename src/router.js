09b97b8110db3d2398ab8b33bd9b1707b4ea6b44
aajour <ahmed.ajourr@gmail.com>
2017-07-31 11:07:48 +0300

    new begining

---
+++ src/router.js
@@ -0,0 +1,23 @@
+    var handlers = require('./handlers.js')
+    var types = {
+        css: 'text/css',
+        jpg: 'image/jpeg',
+        js: 'application/javascript',
+        ico: 'image/x-icon'
+    }
+
+    function router(req, res) {
+        var endpoint = req.url;
+
+        var path = endpoint.split('.');
+        var fileType = types[path[path.length - 1]]
+
+        if (endpoint == '/') {
+            handlers.handleHome(req, res);
+        } else if (endpoint == "/create-post") {
+            handlers.handlePost(req, res);
+        } else {
+            handlers.handlePublic(req, res);
+        }
+    }
+    module.exports = router;
--- src/server.js
+++ src/server.js
@@ -1,57 +1,6 @@
-var http = require('http');
-var fs = require('fs')
-var querystring = require('querystring');
-var message = "Hello world! :D"
-var types = {
-    css: 'text/css',
-    jpg: 'image/jpeg',
-    js: 'application/javascript',
-    ico: 'image/x-icon'
-}
-var server = http.createServer(handler);
-server.listen(5000, function() {
-    console.log("it's working!");
-})
-
-function handler(req, res) {
-    var endpoint = req.url;
-    var path = endpoint.split('.');
-    var fileType = types[path[path.length - 1]]
-
-    if (endpoint == '/') {
-        res.writeHead(200, { 'Conten-Type': 'text/html' });
-        fs.readFile(__dirname + '/../public/index.html', function(err, content) {
-            if (err) {
-                res.end("<h1> Server down </h1>")
-                return;
-            }
-            res.end(content);
-        })
-    } else if (endpoint == "/create-post") {
-        var allTheData = "";
-        req.on('data', function(chunkOfData) {
-            allTheData += chunkOfData;
-        })
-        req.on('end', function() {
-            var convertedData = querystring.parse(allTheData);
-            console.log(convertedData.blogbost);
-            res.writeHead(301, { 'Location': '/' })
-            res.end();
-
-        })
-
-    } else {
-        fs.readFile(__dirname + '/../public' + endpoint, function(err, content) {
-            console.log(fileType, endpoint)
-            if (err) {
-                res.writeHead(200, { 'Conten-Type': 'text/html' });
-                res.end("<h1>Sever down</h1>")
-                console.log("this is also happining")
-                return;
-            }
-            res.writeHead(200, { 'Content-Type': fileType })
-            res.end(content);
-        })
-
-    }
-}
+// var http = require('http');
+// var router = require('./router.js')
+// var server = http.createServer(router);
+// server.listen(5000, function() {
+//     console.log("it's working!");
+// })
